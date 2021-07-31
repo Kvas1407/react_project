70ae810b4e7e8f93ed20631753e66d72ded8a64a
keerthivasan <77402472+Kvas1407@users.noreply.github.com>
2021-07-30 22:41:08 -0400

    Modified the Product image URL link

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,7 +5,7 @@ const ProductList = ({data}) => {
   const {productName,productDescp,productPrice,productImage} = data
 
   const [img, setImg] = useState(``)
-  import (`img/${productImage}`).then((image) => setImg(image.default))
+  import (`../img/Product-images/${productImage}`).then((image) => setImg(image.default))
 
   return (
 
@@ -13,7 +13,7 @@ const ProductList = ({data}) => {
      <header>
             <a href="product.html" target="_blank"><img src={img} alt={productDescp} className="img-shoe"/></a>
             <h3 className="h3-heading">{productName}</h3>
-            <data value="39" className="para-product"><del>$90.00</del> <ins>{productPrice}</ins></data>
+            <data value="39" className="para-product"><del>$90.00</del> <ins>${productPrice}</ins></data>
             <p className="para-product">Here is a shot of this product that might entice a user to click and add it to their cart.</p>
             <dl>
               <dt className="filtertype">Rating</dt>
