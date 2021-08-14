5043497bdb9eb0e2aa172349883274f9423df06e
keerthivasan <77402472+Kvas1407@users.noreply.github.com>
2021-08-14 12:40:11 -0400

    Included the pagination for the webpage

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,28 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+var cache = window.localStorage;
 
-const ProductList = ({data}) => {
+const ProductList = ({ data }) => {
 
-  const {productName,productDescp,productPrice,productImage} = data
+
+  // let cartCount = cache.getItem('cartCount');
+  // let wishlistCount = 0;
+
+  // const addToCartClicked = () => {
+  //   cartCount + 1;
+  //   cache.setItem('cartCount', cartCount);
+  //   console.log(cartCount);
+  // }
+
+  const { productName, productDescp, productPrice, productImage } = data
 
   const [img, setImg] = useState(``)
-  import (`../img/Product-images/${productImage}`).then((image) => setImg(image.default))
+  import(`../img/${productImage}`).then((image) => setImg(image.default))
 
   return (
 
-<article className="product"> 
-     <header>
-            <a href="product.html" target="_blank"><img src={img} alt={productDescp} className="img-shoe"/></a>
-            <h3 className="h3-heading">{productName}</h3>
-            <data value="39" className="para-product"><del>$90.00</del> <ins>${productPrice}</ins></data>
-            <p className="para-product">Here is a shot of this product that might entice a user to click and add it to their cart.</p>
-            <dl>
-              <dt className="filtertype">Rating</dt>
-              <dd>4.4 <span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star_half</span></dd>
-            </dl>
-          </header>
-          </article>
+    <article className="product">
+      <header>
+        <a href="product.html" target="_blank"><img  src={img} alt={productDescp} className="img-shoe" /></a>
+        <h3 className="h3-heading">{productName}</h3>
+        <data value="39" className="para-product"><del>$90.00</del> <ins>${productPrice}</ins></data>
+        <p className="para-product">Here is a shot of this product that might entice a user to click and add it to their cart.</p>
+        {/* <div>
+          <input type='button' value="Add To Cart" onClick={addToCartClicked()} style={{ color: 'white', backgroundColor: '#D54756' }}></input>
+          <input type='button' value="Add to WishList" style={{ color: 'white', backgroundColor: '#D54756', marginLeft: '3px' }}></input>
+        </div> */}
+        <dl>
+          <dt className="filtertype">Rating</dt>
+          <dd>4.4 <span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star_half</span></dd>
+        </dl>
+      </header>
+    </article>
 
-          )
+  )
 }
 
 export default ProductList
