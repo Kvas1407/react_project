d5b64daf9afa5224e090f3f756197dafa02e3f90
keerthivasan <77402472+Kvas1407@users.noreply.github.com>
2021-08-14 12:40:32 -0400

    Sorted out the pagination

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProductList from './ProductList'
 
-const SearchFilter = ({result}) => {
+
+const SearchFilter = ({ result }) => {
+
+    const [currPage, setCurrPage] = useState(1)
+    const pageSize = 6;
+
+    const startRow = (currPage - 1) * pageSize
+    const endRow = startRow + pageSize
+    const totalPages = Math.ceil(result.length / pageSize)
 
     console.log('products', result);
-    const ProductArray = result.slice(0,20).map((product) => <ProductList key={product.prod_name} data={product} />)
-    
-    
+    const ProductArray = result.slice(startRow, endRow).map((product) => <ProductList key={product.prod_name} data={product} />)
+
+    const updatePage = (page) => {
+        if (page < 0)
+            setCurrPage(1)
+        else if (page > totalPages)
+            setCurrPage(totalPages)
+        else
+            setCurrPage(page)
+    }
 
     return (
 
         <div className="results">
             {ProductArray}
 
-            <p id="pageNumber">
-               
-                {ProductArray.length} {(ProductArray.length === 1) ? `product` : `products`} of {result.length}
-
-                
-            </p>
+            <div style ={{alignContent:'center'}}>
+                <p id="numProducts" style={{ backgroundColor: `Beige` }}>
+                    <button onClick={() => updatePage(currPage - 1)} disabled={(currPage === 1) ? `disabled` : ``}>
+                        <span className="material-icons">{'Previous Page <'}</span>
+                    </button>
+                    Showing {(ProductArray.length === 1) ? `product` : `products`} {startRow + 1} to {Math.min(endRow, result.length)} of {result.length}
+                    <button onClick={() => updatePage(currPage + 1)} disabled={(currPage === totalPages) ? `disabled` : ``} >
+                        <span className="material-icons">{'Next Page >'}</span>
+                    </button>
+                </p>
+            </div>
         </div>
     )
 }
