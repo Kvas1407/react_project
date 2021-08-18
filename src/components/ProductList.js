28965e6bda40b998fd27dc15d749411f8da9572f
keerthivasan <77402472+Kvas1407@users.noreply.github.com>
2021-08-18 18:32:44 -0400

    Cleared the Add to cart and Favourite

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,10 +9,10 @@ const ProductList = ({ data }) => {
   import(`../img/${productImage}`).then((image) => setImg(image.default))
 
   return (
-
+    
     <article className="product">
       <header>
-        <a href="product.html" target="_blank"><img  src={img} alt={productDescp} className="img-shoe" /></a>
+                <a href="product.html" target="_blank"><img  src={img} alt={productDescp} className="img-shoe" /></a>
         <h3 className="h3-heading">{productName}</h3>
         <data value="39" className="para-product"><del>$90.00</del> <ins>${productPrice}</ins></data>
         <p className="para-product">Here is a shot of this product that might entice a user to click and add it to their cart.</p>
